1ce91c1ac7e8715f1a0ba9708db1a72cf55d96a1
acmeplus <ctalon@gmail.com>
2024-01-05 21:05:15 +0000

    Fixed mali g31 fbdev missing includes and incorrect links

--- package/batocera/gpu/mali-g31-fbdev/mali-g31-fbdev.mk
+++ package/batocera/gpu/mali-g31-fbdev/mali-g31-fbdev.mk
@@ -14,13 +14,16 @@ define MALI_G31_FBDEV_INSTALL_STAGING_CMDS
 		$(STAGING_DIR)/usr/lib/libmali.so
 
 	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so libMali.so)
+	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so libmali.so.0)
 	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so libmali.so.1)
 	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so libEGL.so)
 	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so libEGL.so.1)
 	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so libGLESv1_CM.so)
 	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so libGLESv1_CM.so.1)
 	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so libGLESv2.so)
-	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so libGLESv2.so.1)
+	(cd $(STAGING_DIR)/usr/lib && ln -sf libmali.so libGLESv2.so.2)
+
+        cp -rf $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/gpu/mali-g31-fbdev/include/* $(STAGING_DIR)/usr/include/
 
         $(INSTALL) -D -m 0644  $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/gpu/mali-g31-fbdev/egl.pc \
                 $(STAGING_DIR)/usr/lib/pkgconfig/egl.pc
@@ -36,13 +39,14 @@ define MALI_G31_FBDEV_INSTALL_TARGET_CMDS
 		$(TARGET_DIR)/usr/lib/libmali.so
 
 	(cd $(TARGET_DIR)/usr/lib && ln -sf libmali.so libMali.so)
+	(cd $(TARGET_DIR)/usr/lib && ln -sf libmali.so libmali.so.0)
 	(cd $(TARGET_DIR)/usr/lib && ln -sf libmali.so libmali.so.1)
 	(cd $(TARGET_DIR)/usr/lib && ln -sf libmali.so libEGL.so)
 	(cd $(TARGET_DIR)/usr/lib && ln -sf libmali.so libEGL.so.1)
 	(cd $(TARGET_DIR)/usr/lib && ln -sf libmali.so libGLESv1_CM.so)
 	(cd $(TARGET_DIR)/usr/lib && ln -sf libmali.so libGLESv1_CM.so.1)
 	(cd $(TARGET_DIR)/usr/lib && ln -sf libmali.so libGLESv2.so)
-	(cd $(TARGET_DIR)/usr/lib && ln -sf libmali.so libGLESv2.so.21
+	(cd $(TARGET_DIR)/usr/lib && ln -sf libmali.so libGLESv2.so.2)
 endef
 
 $(eval $(generic-package))
