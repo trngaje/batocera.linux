40c6a93e473fc5aae8b298a01246dd67275aa5b8
trngaje <bilbobaggins@naver.com>
2024-03-24 10:22:48 +0900

    replace es menu and retroarch font with nanum gothic replace package/batocera/fonts/nanum-font/NanumMyeongjo.ttf with nanum gothic

--- package/batocera/fonts/nanum-font/nanum_font.mk
+++ package/batocera/fonts/nanum-font/nanum_font.mk
@@ -11,7 +11,7 @@ NANUM_FONT_TARGET_DIR=$(TARGET_DIR)/usr/share/fonts/truetype/nanum
 
 define NANUM_FONT_INSTALL_TARGET_CMDS
 	@mkdir -p $(NANUM_FONT_TARGET_DIR)
-	@cp $(@D)/ttf/NanumMyeongjo.ttf $(NANUM_FONT_TARGET_DIR)
+	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/fonts/nanum-font/NanumMyeongjo.ttf $(NANUM_FONT_TARGET_DIR)
 endef
 
 $(eval $(generic-package))
