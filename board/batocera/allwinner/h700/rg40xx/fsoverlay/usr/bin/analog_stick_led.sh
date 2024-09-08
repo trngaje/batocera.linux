ea89a9f36fcb023ca592d09caa9e90bfbc73ad05
Chrizzo <chrizzo@unterderbruecke.de>
2024-09-08 23:28:34 +0200

    Simplified batocera.conf params; daemon now watching for changes

--- board/batocera/allwinner/h700/rg40xx/fsoverlay/usr/bin/analog_stick_led.sh
+++ board/batocera/allwinner/h700/rg40xx/fsoverlay/usr/bin/analog_stick_led.sh
@@ -1,40 +1,24 @@
 #!/bin/bash
 
 # Define some keys for batocera.conf
-KEY_LED_MODE="rg40xxh.rgb.mode"
-KEY_BRIGHTNESS="rg40xxh.rgb.brightness"
-KEY_SPEED="rg40xxh.rgb.speed"
-KEY_RED="rg40xxh.rgb.r"
-KEY_GREEN="rg40xxh.rgb.g"
-KEY_BLUE="rg40xxh.rgb.b"
-KEY_LEFT_RED="rg40xxh.rgb.r.left"
-KEY_LEFT_GREEN="rg40xxh.rgb.g.left"
-KEY_LEFT_BLUE="rg40xxh.rgb.b.left"
-KEY_RIGHT_RED="rg40xxh.rgb.r.right"
-KEY_RIGHT_GREEN="rg40xxh.rgb.g.right"
-KEY_RIGHT_BLUE="rg40xxh.rgb.b.right"
+KEY_LED_MODE="led.mode"
+KEY_LED_BRIGHTNESS="led.brightness"
+KEY_LED_SPEED="led.speed"
+KEY_LED_COLOUR="led.colour"
+KEY_LED_COLOUR_RIGHT="led.colour.right"
 
 # Define some default values
 DEFAULT_LED_MODE=1
 DEFAULT_BRIGHTNESS=100
 DEFAULT_SPEED=1
-DEFAULT_RED=80
-DEFAULT_GREEN=120
-DEFAULT_BLUE=10
+DEFAULT_COLOUR=(80 120 10)
 
 # Read settings from batocera.conf
 LED_MODE=$(batocera-settings-get $KEY_LED_MODE)
-BRIGHTNESS=$(batocera-settings-get $KEY_BRIGHTNESS)
-SPEED=$(batocera-settings-get $KEY_SPEED)
-R=$(batocera-settings-get $KEY_RED)
-G=$(batocera-settings-get $KEY_GREEN)
-B=$(batocera-settings-get $KEY_BLUE)
-LEFT_R=$(batocera-settings-get $KEY_LEFT_RED)
-LEFT_G=$(batocera-settings-get $KEY_LEFT_GREEN)
-LEFT_B=$(batocera-settings-get $KEY_LEFT_BLUE)
-RIGHT_R=$(batocera-settings-get $KEY_RIGHT_RED)
-RIGHT_G=$(batocera-settings-get $KEY_RIGHT_GREEN)
-RIGHT_B=$(batocera-settings-get $KEY_RIGHT_BLUE)
+BRIGHTNESS=$(batocera-settings-get $KEY_LED_BRIGHTNESS)
+SPEED=$(batocera-settings-get $KEY_LED_SPEED)
+COLOUR=($(batocera-settings-get $KEY_LED_COLOUR))
+COLOUR_RIGHT=($(batocera-settings-get $KEY_LED_COLOUR_RIGHT))
 
 # Define the serial device
 SERIAL_DEVICE="/dev/ttyS5"
@@ -60,8 +44,8 @@ fi
 # Set default brightness if no brightness selected or selected brightness is invalid
 if [ -z $BRIGHTNESS ] || [ $BRIGHTNESS -lt 0 ] || [ $BRIGHTNESS -gt 255 ]; then
   echo "Invalid or missing LED brightness - setting LED brightness to default ($DEFAULT_BRIGHTNESS)"
-  batocera-settings-set $KEY_BRIGHTNESS $DEFAULT_BRIGHTNESS
-  BRIGHTNESS=$(batocera-settings-get $KEY_BRIGHTNESS)
+  batocera-settings-set $KEY_LED_BRIGHTNESS $DEFAULT_BRIGHTNESS
+  BRIGHTNESS=$(batocera-settings-get $KEY_LED_BRIGHTNESS)
 fi
 
 # Function to calculate checksum
@@ -120,8 +104,8 @@ elif [ $LED_MODE -ge 5 ] && [ $LED_MODE -le 6 ]; then
   # Ensure speed is provided for modes 5 and 6 and within the valid range (0-255)
   if [ -z $SPEED ] || [ $SPEED -lt 0 ] || [ $SPEED -gt 255 ]; then
     echo "Invalid or missing LED speed - setting LED speed to default ($DEFAULT_SPEED)"
-    batocera-settings-set $KEY_SPEED $DEFAULT_SPEED
-    SPEED=$(batocera-settings-get $KEY_SPEED)
+    batocera-settings-set $KEY_LED_SPEED $DEFAULT_SPEED
+    SPEED=$(batocera-settings-get $KEY_LED_SPEED)
   fi
 
   # Calculate the checksum
@@ -132,79 +116,35 @@ elif [ $LED_MODE -ge 5 ] && [ $LED_MODE -le 6 ]; then
 
 else
 
-  # Ensure red value is provided for mode 1-4 and within valid range, set to default if not
-  if ([ -z $R ] || [ $R -lt 0 ] || [ $R -gt 255 ]) && ([ -z $LEFT_R ] || [ $LEFT_R -lt 0 ] || [ $LEFT_R -gt 255 ] || [ -z $RIGHT_R ] || [ $RIGHT_R -lt 0 ] || [ $RIGHT_R -gt 255 ]); then
-    echo "Invalid or missing LED left red - setting LED left red to default ($DEFAULT_RED)"
-    batocera-settings-set $KEY_RED $DEFAULT_RED
-    R=$(batocera-settings-get $KEY_RED)
+  # Ensure RGB colours for modes 1-4 are set and within valid range, set to default if not
+  if [ -z $COLOUR ] || [ "${#COLOUR[@]}" -lt 3 ] || [ -z ${COLOUR[0]} ] || [ ${COLOUR[0]} -lt 0 ] || [ ${COLOUR[0]} -gt 255 ] || [ -z ${COLOUR[1]} ] || [ ${COLOUR[1]} -lt 0 ] || [ ${COLOUR[1]} -gt 255 ] || [ -z ${COLOUR[2]} ] || [ ${COLOUR[2]} -lt 0 ] || [ ${COLOUR[2]} -gt 255 ]; then
+    echo "Invalid or missing LED colours - setting LED colours to default (${DEFAULT_COLOUR[@]})"
+    batocera-settings-set $KEY_LED_COLOUR "$(printf "%s" "${DEFAULT_COLOUR[*]}")"
+    COLOUR=($(batocera-settings-get $KEY_LED_COLOUR))
   fi
 
-  # Ensure green value is provided for mode 1-4 and within valid range, set to default if not
-  if ([ -z $G ] || [ $G -lt 0 ] || [ $G -gt 255 ]) && ([ -z $LEFT_G ] || [ $LEFT_G -lt 0 ] || [ $LEFT_G -gt 255 ] || [ -z $RIGHT_G ] || [ $RIGHT_G -lt 0 ] || [ $RIGHT_G -gt 255 ]); then
-    echo "Invalid or missing LED left green - setting LED left green to default ($DEFAULT_GREEN)"
-    batocera-settings-set $KEY_GREEN $DEFAULT_GREEN
-    G=$(batocera-settings-get $KEY_GREEN)
+  # Determine if overrides for the right sticks exist and are within valid range, ignore them if not
+  if [ -z $COLOUR_RIGHT ] || [ "${#COLOUR_RIGHT[@]}" -lt 3 ] || [ -z ${COLOUR_RIGHT[0]} ] || [ ${COLOUR_RIGHT[0]} -lt 0 ] || [ ${COLOUR_RIGHT[0]} -gt 255 ] || [ -z ${COLOUR_RIGHT[1]} ] || [ ${COLOUR_RIGHT[1]} -lt 0 ] || [ ${COLOUR_RIGHT[1]} -gt 255 ] || [ -z ${COLOUR_RIGHT[2]} ] || [ ${COLOUR_RIGHT[2]} -lt 0 ] || [ ${COLOUR_RIGHT[2]} -gt 255 ]; then
+    echo "No LED colour overrides for the right stick found - using regular colors on the right stick (${DEFAULT_COLOUR[@]})"
+    COLOUR_RIGHT=(${COLOUR[0]} ${COLOUR[1]} ${COLOUR[2]})
   fi
 
-  # Ensure blue value is provided for mode 1-4 and within valid range, set to default if not
-  if ([ -z $B ] || [ $B -lt 0 ] || [ $B -gt 255 ]) && ([ -z $LEFT_B ] || [ $LEFT_B -lt 0 ] || [ $LEFT_B -gt 255 ] || [ -z $RIGHT_B ] || [ $RIGHT_B -lt 0 ] || [ $RIGHT_B -gt 255 ]); then
-    echo "Invalid or missing LED left green - setting LED left green to default ($DEFAULT_BLUE)"
-    batocera-settings-set $KEY_BLUE $DEFAULT_BLUE
-    B=$(batocera-settings-get $KEY_BLUE)
-  fi
-
-  # Ensure left red value is provided for mode 1-4 and within valid range
-  if [ -z $LEFT_R ] || [ $LEFT_R -lt 0 ] || [ $LEFT_R -gt 255 ]; then
-    echo "No LED left red override found - using regular red ($R)"
-    LEFT_R=$R
-  fi
-
-  # Ensure left green value is provided for mode 1-4 and within valid range
-  if [ -z $LEFT_G ] || [ $LEFT_G -lt 0 ] || [ $LEFT_G -gt 255 ]; then
-    echo "No LED left green override found - using regular green ($G)"
-    LEFT_G=$G
-  fi
-
-  # Ensure left blue value is provided for mode 1-4 and within valid range
-  if [ -z $LEFT_B ] || [ $LEFT_B -lt 0 ] || [ $LEFT_B -gt 255 ]; then
-    echo "No LED left blue override found - using regular blue ($B)"
-    LEFT_B=$B
-  fi
-
-  # Ensure right red value is provided for mode 1-4 and within valid range
-  if [ -z $RIGHT_R ] || [ $RIGHT_R -lt 0 ] || [ $RIGHT_R -gt 255 ]; then
-    echo "No LED right red override found - using regular red ($R)"
-    RIGHT_R=$R
-  fi
-
-  # Ensure right green value is provided for mode 1-4 and within valid range
-  if [ -z $RIGHT_G ] || [ $RIGHT_G -lt 0 ] || [ $RIGHT_G -gt 255 ]; then
-    echo "No LED right green override found - using regular green ($G)"
-    RIGHT_G=$G
-  fi
-
-  # Ensure right blue value is provided for mode 1-4 and within valid range
-  if [ -z $RIGHT_B ] || [ $RIGHT_B -lt 0 ] || [ $RIGHT_B -gt 255 ]; then
-    echo "No LED right blue override found - using regular blue ($B)"
-    RIGHT_B=$B
-  fi
-
-  # Construct the payload for jeft and right joystick values
+  # Construct the payload for left and right joystick values
   PAYLOAD=$(printf '\\x%02X\\x%02X' $LED_MODE $BRIGHTNESS)
   for ((i = 0; i < 8; i++)); do
-    PAYLOAD+=$(printf '\\x%02X\\x%02X\\x%02X' $RIGHT_R $RIGHT_G $RIGHT_B)
+    PAYLOAD+=$(printf '\\x%02X\\x%02X\\x%02X' ${COLOUR_RIGHT[0]} ${COLOUR_RIGHT[1]} ${COLOUR_RIGHT[2]})
   done
   for ((i = 0; i < 8; i++)); do
-    PAYLOAD+=$(printf '\\x%02X\\x%02X\\x%02X' $LEFT_R $LEFT_G $LEFT_B)
+    PAYLOAD+=$(printf '\\x%02X\\x%02X\\x%02X' ${COLOUR[0]} ${COLOUR[1]} ${COLOUR[2]})
   done
 
   # Calculate checksum for the payload
   PAYLOAD_BYTES=($LED_MODE $BRIGHTNESS)
   for ((i = 0; i < 8; i++)); do
-    PAYLOAD_BYTES+=($RIGHT_R $RIGHT_G $RIGHT_B)
+    PAYLOAD_BYTES+=(${COLOUR_RIGHT[0]} ${COLOUR_RIGHT[1]} ${COLOUR_RIGHT[2]})
   done
   for ((i = 0; i < 8; i++)); do
-    PAYLOAD_BYTES+=($LEFT_R $LEFT_G $LEFT_B)
+    PAYLOAD_BYTES+=(${COLOUR[0]} ${COLOUR[1]} ${COLOUR[2]})
   done
   CHECKSUM=$(calculate_checksum "${PAYLOAD_BYTES[@]}")
   PAYLOAD+=$(printf '\\x%02X' $CHECKSUM)
--- board/batocera/allwinner/h700/rg40xx/fsoverlay/usr/bin/analog_stick_led_daemon.sh
+++ board/batocera/allwinner/h700/rg40xx/fsoverlay/usr/bin/analog_stick_led_daemon.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+# Define some keys for batocera.conf
+KEY_LED_MODE="led.mode"
+KEY_BRIGHTNESS="led.brightness"
+KEY_SPEED="led.speed"
+KEY_LED_COLOUR="led.colour"
+KEY_LED_COLOUR_RIGHT="led.colour.right"
+
+# Last known RGB settings
+LAST_LED_MODE=-1
+LAST_BRIGHTNESS=-1
+LAST_SPEED=-1
+LAST_COLOUR=-1
+LAST_COLOUR_RIGHT=-1
+
+# Last change date
+LAST_CHANGE_DATE=-1
+
 # Sleep interval for daemon
-UPDATE_INTERVAL_SECONDS=60
+UPDATE_INTERVAL_SECONDS=1
 
 # Battery charge thresholds (inclusive) for warning/danger
 THRESHOLD_WARNING=20
@@ -13,9 +30,38 @@ MODE_DANGER=1
 MODE_DEFAULT=0
 
 CURRENT_MODE=-1
+RGB_CHANGE_DETECTED=false
+
+detectRgbChanges() {
+	CURRENT_CHANGE_DATE=$(date -r "/userdata/system/batocera.conf")
+	# Only check for details if batocera.conf has recently been changed
+	if [ "$LAST_CHANGE_DATE" != "$CURRENT_CHANGE_DATE" ]; then
+		LAST_CHANGE_DATE=$CURRENT_CHANGE_DATE
+		LED_MODE=$(batocera-settings-get $KEY_LED_MODE)
+		BRIGHTNESS=$(batocera-settings-get $KEY_BRIGHTNESS)
+		SPEED=$(batocera-settings-get $KEY_SPEED)
+		COLOUR=($(batocera-settings-get $KEY_LED_COLOUR))
+		COLOUR_RIGHT=($(batocera-settings-get $KEY_LED_COLOUR_RIGHT))
+		if [ $LED_MODE -ne $LAST_LED_MODE ] ||
+			[ $BRIGHTNESS -ne $LAST_BRIGHTNESS ] || 
+			[ $SPEED -ne $LAST_SPEED ] ||
+			[ "$(printf "%s" "${COLOUR[*]}")" != "$(printf "%s" "${LAST_COLOUR[*]}")" ] ||
+			[ "$(printf "%s" "${COLOUR_RIGHT[*]}")" != "$(printf "%s" "${LAST_COLOUR_RIGHT[*]}")" ]; then
+			RGB_CHANGE_DETECTED=true
+			echo "Change in RGB LED settings detected!"
+		fi
+		LAST_LED_MODE=$LED_MODE
+		LAST_BRIGHTNESS=$BRIGHTNESS
+		LAST_SPEED=$SPEED
+		LAST_COLOUR=$COLOUR
+		LAST_COLOUR_RIGHT=$COLOUR_RIGHT
+	fi
+}
 
 daemon() {
   while :; do
+  	# Detect RGB changes
+  	detectRgbChanges
   
     # Determine current battery charge
     BATTERY_CHARGE=$(batocera-info | grep "Battery" | sed -e "s/^Battery: //" -e "s/%$//")
@@ -31,10 +77,11 @@ daemon() {
       /usr/bin/analog_stick_led.sh danger
       CURRENT_MODE=$MODE_DANGER
     # Go back to normal LED mode if set to either warning or danger but battery status is above warning threshold
-    elif [ $CURRENT_MODE -ne $MODE_DEFAULT ] && [ $BATTERY_CHARGE -gt $THRESHOLD_WARNING ]; then
+    elif ($RGB_CHANGE_DETECTED || [ $CURRENT_MODE -ne $MODE_DEFAULT ]) && [ $BATTERY_CHARGE -gt $THRESHOLD_WARNING ]; then
       echo "Battery charge at $BATTERY_CHARGE - Going to normal LED mode"
       /usr/bin/analog_stick_led.sh
       CURRENT_MODE=$MODE_DEFAULT
+      RGB_CHANGE_DETECTED=false
     fi
   
     # Sleep until interval is over
