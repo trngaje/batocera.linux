1c6b33bace7ef7c2a53da2a260cc794492c90a23
Chrizzo <chrizzo@unterderbruecke.de>
2024-09-16 00:30:12 +0200

    Allowing to override batocera.conf settings via command line

--- board/batocera/allwinner/h700/rg40xx/fsoverlay/usr/bin/analog_stick_led_daemon.sh
+++ board/batocera/allwinner/h700/rg40xx/fsoverlay/usr/bin/analog_stick_led_daemon.sh
@@ -7,11 +7,34 @@ KEY_LED_SPEED="led.speed"
 KEY_LED_COLOUR="led.colour"
 KEY_LED_COLOUR_RIGHT="led.colour.right"
 
+# Paths to variables
+VAR_PID="/var/run/analog_stick_led_daemon.pid"
+VAR_CONF_CHANGE_DATE="/var/run/analog_stick_led_daemon.date"
+VAR_MODE="/var/run/analog_stick_led_daemon.mode"
+VAR_BRIGHTNESS="/var/run/analog_stick_led_daemon.brightness"
+VAR_SPEED="/var/run/analog_stick_led_daemon.speed"
+VAR_COLOUR="/var/run/analog_stick_led_daemon.colour"
+VAR_COLOUR_RIGHT="/var/run/analog_stick_led_daemon.colour_right"
+
+# Sleep interval for daemon
+UPDATE_INTERVAL_SECONDS=1
+
+# Constants for different LED Daemon modes (different from LED modes!)
+MODE_CHARGING=3
+MODE_WARNING=2
+MODE_DANGER=1
+MODE_DEFAULT=0
+
 # Define some default RGB LED settings
 DEFAULT_LED_MODE=1
 DEFAULT_BRIGHTNESS=100
-DEFAULT_SPEED=1
+DEFAULT_SPEED=5
 DEFAULT_COLOUR=(80 120 10)
+
+# Colours and mode for low battery
+BATTERY_WARNING_MODE=2
+BATTERY_WARNING_COLOUR=(255 255 0)
+BATTERY_DANGER_COLOUR=(255 0 0)
 
 # Paths to battery values
 KEY_BATTERY_CAPACITY="/sys/class/power_supply/axp2202-battery/capacity"
@@ -22,36 +45,122 @@ BATTERY_CHARGING="Charging"
 BATTERY_DISCHARGING="Discharging"
 BATTERY_FULL="Full"
 
-# Colours and mode for low battery
-BATTERY_WARNING_MODE=2
-BATTERY_WARNING_COLOUR=(255 255 0)
-BATTERY_DANGER_COLOUR=(255 0 0)
-
 # Low battery charge thresholds (inclusive) for warning/danger
 THRESHOLD_WARNING=20
 THRESHOLD_DANGER=5
 
-# Last known RGB LED settings
+# Initialize last known RGB LED settings
 LAST_LED_MODE=-1
 LAST_BRIGHTNESS=-1
 LAST_SPEED=-1
 LAST_COLOUR=-1
 LAST_COLOUR_RIGHT=-1
 
-# Last change date
-LAST_CHANGE_DATE=-1
+# Initialize last known change dates
+LAST_CONF_CHANGE_DATE=-1
+LAST_MODE_CHANGE_DATE=-1
+LAST_BRIGHTNESS_CHANGE_DATE=-1
+LAST_SPEED_CHANGE_DATE=-1
+LAST_COLOUR_CHANGE_DATE=-1
+LAST_COLOUR_RIGHT_CHANGE_DATE=-1
 
-# Sleep interval for daemon
-UPDATE_INTERVAL_SECONDS=1
+# Recover last known change date of batocera.conf (relevant for recovering from reset)
+if [ -f "$VAR_CONF_CHANGE_DATE" ]; then
+  LAST_CONF_CHANGE_DATE=$(cat $VAR_CONF_CHANGE_DATE)
+fi
 
-# Constants for different modes
-MODE_CHARGING=3
-MODE_WARNING=2
-MODE_DANGER=1
-MODE_DEFAULT=0
-
+# Initialize current state variables
 CURRENT_MODE=-1
 LED_SETTINGS_CHANGE_DETECTED=false
+
+readLedVariables() {
+
+  if [ -f "$VAR_MODE" ] && [ "$LAST_MODE_CHANGE_DATE" != "$(date -r $VAR_MODE)" ]; then
+    LAST_LED_MODE=$(cat $VAR_MODE)
+    LAST_MODE_CHANGE_DATE=$(date -r $VAR_MODE)
+    LED_SETTINGS_CHANGE_DETECTED=true
+  fi
+  
+  if [ -f "$VAR_BRIGHTNESS" ] && [ "$LAST_BRIGHTNESS_CHANGE_DATE" != "$(date -r $VAR_BRIGHTNESS)" ]; then
+    LAST_BRIGHTNESS=$(cat $VAR_BRIGHTNESS)
+    LAST_BRIGHTNESS_CHANGE_DATE=$(date -r $VAR_BRIGHTNESS)
+    LED_SETTINGS_CHANGE_DETECTED=true
+  fi
+  
+  if [ -f "$VAR_SPEED" ] && [ "$LAST_SPEED_CHANGE_DATE" != "$(date -r $VAR_SPEED)" ]; then
+    LAST_SPEED=$(cat $VAR_SPEED)
+    LAST_SPEED_CHANGE_DATE=$(date -r $VAR_SPEED)
+    LED_SETTINGS_CHANGE_DETECTED=true
+  fi
+
+  CURRENT_COLOUR_CHANGE_DATE=
+  if [ -f "$VAR_COLOUR" ] && [ "$LAST_COLOUR_CHANGE_DATE" != "$(date -r $VAR_COLOUR)" ]; then
+    LAST_COLOUR=($(cat $VAR_COLOUR))
+    LAST_COLOUR_CHANGE_DATE=$(date -r $VAR_COLOUR)
+    LED_SETTINGS_CHANGE_DETECTED=true
+  fi
+
+  if [ -f "$VAR_COLOUR_RIGHT" ] && [ "$LAST_COLOUR_RIGHT_CHANGE_DATE" != "$(date -r $VAR_COLOUR_RIGHT)" ]; then
+    LAST_COLOUR_RIGHT=($(cat $VAR_COLOUR_RIGHT))
+    LAST_COLOUR_RIGHT_CHANGE_DATE=$(date -r $VAR_COLOUR_RIGHT)
+    LED_SETTINGS_CHANGE_DETECTED=true
+  fi
+
+}
+
+# Sets LED variables to given values.
+setLedVariables() {
+
+  echo $1 > $VAR_MODE
+  echo $2 > $VAR_BRIGHTNESS
+  echo $3 > $VAR_SPEED
+  echo "$4 $5 $6" > $VAR_COLOUR
+  echo "$7 $8 $9" > $VAR_COLOUR_RIGHT
+
+}
+
+# Clears all LED variables by removing the files.
+clearLedVariables() {
+
+  LAST_LED_MODE=-1
+  LAST_BRIGHTNESS=-1
+  LAST_SPEED=-1
+  LAST_COLOUR=-1
+  LAST_COLOUR_RIGHT=-1
+  LAST_CONF_CHANGE_DATE=-1
+  LAST_MODE_CHANGE_DATE=-1
+  LAST_BRIGHTNESS_CHANGE_DATE=-1
+  LAST_SPEED_CHANGE_DATE=-1
+  LAST_COLOUR_CHANGE_DATE=-1
+  LAST_COLOUR_RIGHT_CHANGE_DATE=-1
+
+  if [ -f "$VAR_MODE" ]; then
+    rm $VAR_MODE
+  fi
+  
+  if [ -f "$VAR_BRIGHTNESS" ]; then
+    rm $VAR_BRIGHTNESS
+  fi
+  
+  if [ -f "$VAR_SPEED" ]; then
+    rm $VAR_SPEED
+  fi
+
+  if [ -f "$VAR_COLOUR" ]; then
+    rm $VAR_COLOUR
+  fi
+
+  if [ -f "$VAR_COLOUR_RIGHT" ]; then
+    rm $VAR_COLOUR_RIGHT
+  fi
+
+  if [ -f "$VAR_CONF_CHANGE_DATE" ]; then
+    rm $VAR_CONF_CHANGE_DATE
+  fi
+  
+  echo "Removed all variables from previous session."
+
+}
 
 # Function which retrieves latest LED settings from batocera.conf.
 # Also puts back valid settings if invalid settings have been made.
@@ -100,7 +209,7 @@ updateLedSettings() {
     COLOUR_RIGHT=(${COLOUR[0]} ${COLOUR[1]} ${COLOUR[2]})
   fi
 
-  # Determine if any RGB LED setting has changed since last check 
+  # Determine if any RGB LED setting has changed since last check.
   if [ $LED_MODE -ne $LAST_LED_MODE ] ||
     [ $BRIGHTNESS -ne $LAST_BRIGHTNESS ] || 
     [ $SPEED -ne $LAST_SPEED ] ||
@@ -108,14 +217,8 @@ updateLedSettings() {
     [ "$(printf "%s" "${COLOUR_RIGHT[*]}")" != "$(printf "%s" "${LAST_COLOUR_RIGHT[*]}")" ]; then
     LED_SETTINGS_CHANGE_DETECTED=true
     echo "Change in RGB LED settings detected!"
+    setLedVariables $LED_MODE $BRIGHTNESS $SPEED ${COLOUR[0]} ${COLOUR[1]} ${COLOUR[2]} ${COLOUR_RIGHT[0]} ${COLOUR_RIGHT[1]} ${COLOUR_RIGHT[2]}
   fi
-
-  # Update last known RGB LED settings
-  LAST_LED_MODE=$LED_MODE
-  LAST_BRIGHTNESS=$BRIGHTNESS
-  LAST_SPEED=$SPEED
-  LAST_COLOUR=(${COLOUR[0]} ${COLOUR[1]} ${COLOUR[2]})
-  LAST_COLOUR_RIGHT=(${COLOUR_RIGHT[0]} ${COLOUR_RIGHT[1]} ${COLOUR_RIGHT[2]})
 
 }
 
@@ -124,8 +227,11 @@ updateLedSettings() {
 detectRgbChanges() {
   CURRENT_CHANGE_DATE=$(date -r "/userdata/system/batocera.conf")
   # Only check for details if batocera.conf has recently been changed
-  if [ "$LAST_CHANGE_DATE" != "$CURRENT_CHANGE_DATE" ]; then
-    LAST_CHANGE_DATE=$CURRENT_CHANGE_DATE
+  if [ "$LAST_CONF_CHANGE_DATE" != "$CURRENT_CHANGE_DATE" ]; then
+    LAST_CONF_CHANGE_DATE=$CURRENT_CHANGE_DATE
+    echo $CURRENT_CHANGE_DATE > $VAR_CONF_CHANGE_DATE
+    echo "Batocera.conf has been changed - updating LED settings."
+    readLedVariables
     updateLedSettings
   fi
 }
@@ -134,6 +240,7 @@ daemon() {
   while :; do
     # Detect RGB changes
     detectRgbChanges
+    readLedVariables
   
     # Determine current battery status
     BATTERY_CHARGE=$(cat $KEY_BATTERY_CAPACITY)
@@ -141,7 +248,7 @@ daemon() {
 
     # Go to LED mode "charging" if the battery is currently charging.
     if [ $CURRENT_MODE -ne $MODE_CHARGING ] && [ $BATTERY_STATUS == $BATTERY_CHARGING ] && [ $BATTERY_CHARGE -lt 100 ]; then
-      echo "Battery charge at $BATTERY_CHARGE - going to LED mode 'warning'"
+      echo "Battery charge at $BATTERY_CHARGE - going to LED mode 'charging'"
       /usr/bin/analog_stick_led.sh $BATTERY_WARNING_MODE $LAST_BRIGHTNESS ${DEFAULT_COLOUR[0]} ${DEFAULT_COLOUR[1]} ${DEFAULT_COLOUR[2]} ${DEFAULT_COLOUR[0]} ${DEFAULT_COLOUR[1]} ${DEFAULT_COLOUR[2]}
       CURRENT_MODE=$MODE_CHARGING
 
@@ -160,6 +267,7 @@ daemon() {
     # Go back to normal LED mode if set to either warning or danger but battery status is above warning threshold
     elif ($LED_SETTINGS_CHANGE_DETECTED || [ $CURRENT_MODE -ne $MODE_DEFAULT ]) && ([ $BATTERY_STATUS == $BATTERY_FULL ] || ([ $BATTERY_STATUS == $BATTERY_DISCHARGING ] && [ $BATTERY_CHARGE -gt $THRESHOLD_WARNING ])); then
       echo "Battery charge at $BATTERY_CHARGE - Going to normal LED mode"
+      readLedVariables
       if [ $LAST_LED_MODE -lt 5 ]; then
         /usr/bin/analog_stick_led.sh $LAST_LED_MODE $LAST_BRIGHTNESS ${LAST_COLOUR_RIGHT[0]} ${LAST_COLOUR_RIGHT[1]} ${LAST_COLOUR_RIGHT[2]} ${LAST_COLOUR[0]} ${LAST_COLOUR[1]} ${LAST_COLOUR[2]}
       else
@@ -176,33 +284,41 @@ daemon() {
 }
 
 start() {
+  if [ $# -eq 1 ] && [ "$1" == "clear" ]; then
+    clearLedVariables
+  fi
   daemon &
   PID=$!
-  echo $PID > /var/run/analog_stick_led_daemon.pid
+  echo $PID > $VAR_PID
   echo "Started analog stick RGB LED daemon."
 }
 
 stop() {
-  kill $(cat /var/run/analog_stick_led_daemon.pid)
+  kill $(cat $VAR_PID)
   /usr/bin/analog_stick_led.sh 0
   echo "Stopped analog stick RGB LED daemon."
 }
 
 restart() {
   stop
-  start
+  start $1
 }
 
 if [ $# -eq 0 ]; then
-  echo "Usage: $0 <start|stop|restart>"
+  echo "Usage: $0 start|restart|stop|set|import [clear|<r_value> <g_value> <b_value>]"
   exit 1
 elif [ "$1" == "start" ]; then
-  start
+  start $2
 elif [ "$1" == "stop" ]; then
   stop
 elif [ "$1" == "restart" ]; then
-  restart
+  restart $2
+elif [ "$1" == "import" ]; then
+  updateLedSettings
+elif [ $# -eq 4 ] && [ "$1" == "set" ]; then
+  BRIGHTNESS=$(batocera-settings-get $KEY_LED_BRIGHTNESS)
+  SPEED=$(batocera-settings-get $KEY_LED_SPEED)
+  setLedVariables 1 $BRIGHTNESS $SPEED $2 $3 $4 $2 $3 $4
 else 
-  echo "Usage: $0 <start|stop|restart|status>"
+  echo "Usage: $0 <start|stop|restart|set [r g b]>"
 fi
-
