becd0f87207b042d10722046039ff1db508fe325
Chrizzo <chrizzo@unterderbruecke.de>
2024-10-20 04:01:36 +0200

    Fixed typo

--- board/batocera/allwinner/h700/fsoverlay/usr/share/batocera/datainit/system/configs/emulationstation/scripts/achievements/led.sh
+++ board/batocera/allwinner/h700/fsoverlay/usr/share/batocera/datainit/system/configs/emulationstation/scripts/achievements/led.sh
@@ -12,7 +12,7 @@ EFFECT_ON=1
 # Check batocera.conf for retroachievement effect setting
 LED_RETRO_ACHIEVEMENTS=$(batocera-settings-get $KEY_LED_RETRO_ACHIEVEMENTS)
 
-# Initialize unsetretroachievement effect setting with default value if necessary
+# Initialize unset retroachievement effect setting with default value if necessary
 if [[ ! -n $LED_RETRO_ACHIEVEMENTS ]] || [ $LED_RETRO_ACHIEVEMENTS -lt 0 ] || [ $LED_RETRO_ACHIEVEMENTS -gt 1 ]; then
   batocera-settings-set $KEY_LED_RETRO_ACHIEVEMENTS $EFFECT_ON
 fi
