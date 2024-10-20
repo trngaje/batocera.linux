a426c05a83850b5271c427b556ae136091068cb9
Chrizzo <chrizzo@unterderbruecke.de>
2024-10-20 04:01:06 +0200

    Only run the rainbow effect if it is turned on in batocera.conf

--- board/batocera/allwinner/h700/fsoverlay/usr/share/batocera/datainit/system/configs/emulationstation/scripts/achievements/led.sh
+++ board/batocera/allwinner/h700/fsoverlay/usr/share/batocera/datainit/system/configs/emulationstation/scripts/achievements/led.sh
@@ -6,5 +6,18 @@ if [ "$BOARD" != "rg40xx-h" ] && [ "$BOARD" != "rg40xx-v" ]; then
     exit 1
 fi
 
-# Let the LED daemon run the rainbow animation
-/usr/bin/analog_stick_led_daemon.sh animation rainbow
+KEY_LED_RETRO_ACHIEVEMENTS="led.retroachievements"
+EFFECT_ON=1
+
+# Check batocera.conf for retroachievement effect setting
+LED_RETRO_ACHIEVEMENTS=$(batocera-settings-get $KEY_LED_RETRO_ACHIEVEMENTS)
+
+# Initialize unsetretroachievement effect setting with default value if necessary
+if [[ ! -n $LED_RETRO_ACHIEVEMENTS ]] || [ $LED_RETRO_ACHIEVEMENTS -lt 0 ] || [ $LED_RETRO_ACHIEVEMENTS -gt 1 ]; then
+  batocera-settings-set $KEY_LED_RETRO_ACHIEVEMENTS $EFFECT_ON
+fi
+
+# Let the LED daemon run the rainbow animation if retroachievement effect is turned on
+if [ $LED_RETRO_ACHIEVEMENTS -eq $EFFECT_ON ]; then
+  /usr/bin/analog_stick_led_daemon.sh animation rainbow
+fi
