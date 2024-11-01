bf9c4842f0b51514fadeab652e03eb6b5fa5a8cb
acmeplus <ctalon@gmail.com>
2024-10-31 21:56:55 -0400

    20241031
    * Added symbuzzer es-theme-knulli to the theme configuration list
    * Updated batocera-bezel
    * Upated build configuration for knulli-bezels

--- package/batocera/emulators/retroarch/knulli-bezels/knulli-bezels.mk
+++ package/batocera/emulators/retroarch/knulli-bezels/knulli-bezels.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 # Version.: Commits on Oct 17, 2024
-BATOCERA_BEZEL_VERSION = dc7097f3e6c09c257d25e9ebf056731f1b3a4ff5
-BATOCERA_BEZEL_SITE = $(call github,chrizzo-hb,knulli-bezels,$(BATOCERA_BEZEL_VERSION))
+KNULLI_BEZELS_VERSION = dc7097f3e6c09c257d25e9ebf056731f1b3a4ff5
+KNULLI_BEZELS_SITE = $(call github,chrizzo-hb,knulli-bezels,$(KNULLI_BEZELS_VERSION))
 
 define KNULLI_BEZELS_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/decorations
