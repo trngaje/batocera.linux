b09bdc27d1c89c06f61f63abe1253823ef2a81df
anthonycaccese <anthonycaccese@gmail.com>
2024-11-13 10:16:14 -0500

    Update Art Book Next
    Adds an initial Extra Large font option (to test on TrimUI Brick and RP Mini)
    Updates system count to have proper pluralization

--- package/batocera/emulationstation/es-theme-art-book-next/es-theme-art-book-next.mk
+++ package/batocera/emulationstation/es-theme-art-book-next/es-theme-art-book-next.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version.: Commits on June 11, 2024
-ES_THEME_ART_BOOK_NEXT_VERSION = b4d4f573ddb790c397d0a78bef8c35cb4e927464
+ES_THEME_ART_BOOK_NEXT_VERSION = 499685f008de725d5ff7c57d4b1c9a8f497677df
 ES_THEME_ART_BOOK_NEXT_SITE = $(call github,anthonycaccese,art-book-next-es,$(ES_THEME_ART_BOOK_NEXT_VERSION))
 
 define ES_THEME_ART_BOOK_NEXT_INSTALL_TARGET_CMDS
