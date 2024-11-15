73d340887a6709be412b0b6901eece5d9f8d46ad
acmeplus <ctalon@gmail.com>
2024-11-15 18:33:59 -0500

    Added mali_fbdev support for retroarch to fix mangohud and other performance issues

--- package/batocera/emulators/retroarch/retroarch/retroarch.mk
+++ package/batocera/emulators/retroarch/retroarch/retroarch.mk
@@ -152,9 +152,9 @@ ifeq ($(BR2_PACKAGE_VULKAN_LOADER)$(BR2_PACKAGE_VULKAN_HEADERS),yy)
     RETROARCH_DEPENDENCIES += vulkan-headers vulkan-loader slang-shaders
 endif
 
-#ifeq ($(BR2_PACKAGE_HAS_LIBMALI),y)
-#	RETROARCH_CONF_OPTS += --enable-mali_fbdev
-#endif
+ifeq ($(BR2_PACKAGE_HAS_LIBMALI),y)
+	RETROARCH_CONF_OPTS += --enable-mali_fbdev
+endif
 
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_ATM7039),y)
 	RETROARCH_CONF_OPTS += --disable-v4l2
