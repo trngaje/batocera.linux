fa868b20fb7c8a4bb9dd16d69e19258a90cebed8
acmeplus <ctalon@gmail.com>
2024-11-26 21:58:47 +0000

    Added knulli splash logos

--- package/batocera/core/batocera-splash/batocera-splash.mk
+++ package/batocera/core/batocera-splash/batocera-splash.mk
@@ -66,10 +66,12 @@ endef
 define BATOCERA_SPLASH_INSTALL_BOOT_LOGO
     mkdir -p $(TARGET_DIR)/usr/share/batocera/splash
     cp "$(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-splash/images/logo.png"      "${TARGET_DIR}/usr/share/batocera/splash/boot-logo.png"
-    cp "$(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-splash/images/logo-half.png"      "${TARGET_DIR}/usr/share/batocera/splash/boot-logo-half.png"
     cp "$(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-splash/images/logo-240.png"  "${TARGET_DIR}/usr/share/batocera/splash/boot-logo-320x240.png"
     cp "$(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-splash/images/logo-480p.png" "${TARGET_DIR}/usr/share/batocera/splash/boot-logo-640x480.png"
     cp "$(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-splash/images/logo-720p.png" "${TARGET_DIR}/usr/share/batocera/splash/boot-logo-1280x720.png"
+    cp "$(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-splash/images/logo-768p.png" "${TARGET_DIR}/usr/share/batocera/splash/boot-logo-1024x768.png"
+    
+    cp "$(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-splash/images/logo-720p-square.png" "${TARGET_DIR}/usr/share/batocera/splash/boot-logo-720x720.png"
 
     cp "$(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-splash/images/logo-3-2-480-rotate.png"	   "${TARGET_DIR}/usr/share/batocera/splash/boot-logo-320x480.png"
     cp "$(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-splash/images/logo-16-9-480-rotate.png"   "${TARGET_DIR}/usr/share/batocera/splash/boot-logo-480x854.png"
