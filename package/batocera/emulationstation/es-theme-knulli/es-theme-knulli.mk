67389f384e9e9f6af431ca3f006e88783d650460
Ali Beyaz <symbuzzer@users.noreply.github.com>
2024-12-09 10:16:23 +0300

    Update es-theme-knulli.mk

--- package/batocera/emulationstation/es-theme-knulli/es-theme-knulli.mk
+++ package/batocera/emulationstation/es-theme-knulli/es-theme-knulli.mk
@@ -3,8 +3,8 @@
 # EmulationStation theme "Knulli"
 #
 ################################################################################
-# Version: Commits on December 07, 2024
-ES_THEME_KNULLI_VERSION = 022333fa02324b60985d02c1c210ed2fad67cfa1
+# Version: Commits on December 09, 2024
+ES_THEME_KNULLI_VERSION = 8cfabf681dfdfae117e02257310ce76a8198ac14
 ES_THEME_KNULLI_SITE = $(call github,symbuzzer,es-theme-knulli,$(ES_THEME_KNULLI_VERSION))
 
 define ES_THEME_KNULLI_INSTALL_TARGET_CMDS
