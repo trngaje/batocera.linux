728bebbc968631546cde8abb931d32fff0b467f3
Mikhailzrick <michael.shryock@gmail.com>
2024-12-11 22:17:10 -0600

    Fix-restart-while-charging
    Sets a flag on restart so that the charger binary is not run if plugged in. Requires ES PR change to call reboot/poweroff scripts if they exist.

--- board/batocera/allwinner/h700/fsoverlay/usr/bin/reboot.sh
+++ board/batocera/allwinner/h700/fsoverlay/usr/bin/reboot.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+mount -o remount,rw /boot
+
+echo "1" > /boot/restart.flag # So the system knows if it's restart or cold boot for the charging binary in rcS
 sync
+
+mount -o remount,ro /boot
 
 reboot
